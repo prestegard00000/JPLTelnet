18d0a46f18956d1fa4577d5530a7abb173c362fc
Aaron Prestegard <prestegard00000@gmail.com>
2022-09-07 04:07:29 -0700

    fixed bugs, calls modual function

--- JPLTelnet.go
+++ JPLTelnet.go
@@ -1,14 +1,23 @@
 package main
 
-import {
-	"../Telnet/Telnet
-}
+import (
+	"fmt"
+
+	"example.com/telnet_interface.go"
+
+)
 
 func main() {
-	var string response
-	var string website = "horizons.jpl.nasa.gov"
-	var int portnumber = 6775
-	
-	connection = Telnet(website, portnumber)
+	var response string = ""
+	var response_error error
+	var website string = "horizons.jpl.nasa.gov"
+	var portnumber int = 6775
 
+	response, response_error = communications.Telnet_connect(website, portnumber)
+
+	if(response_error == "") {
+	
+		fmt.Sprintf("%s",response)
+	
+	}
 }
