aba65fb46a5ef5f9a703440bcc03ff04c84a5f41
Aaron Prestegard <prestegard00000@gmail.com>
2022-09-13 18:26:42 -0700

    syntax|feature  adjusted telnet_interface_go.  adjusting dependency locations

--- JPLTelnet.go
+++ JPLTelnet.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
-
-	"example.com/telnet_interface.go"
+	"github.com/prestegard00000/telnet_interface_go"
+	"error"
+	//"example.com/telnet_interface.go"
 
 )
 
 func main() {
+	var JPLProfile Telnet_profile
+	JPLProfile.portnumber = 6775
+	JPLProfile.site = "horizons.jpl.nasa.gov"
+	//JPLProfile := communications.Telnet_profile {
+//		portNumber: 6775,
+//		site: "horizons.jpl.nasa.gov",
+//	}
+
 	var response string = ""
 	var response_error error
-	var website string = "horizons.jpl.nasa.gov"
-	var portnumber int = 6775
-
-	response, response_error = communications.Telnet_connect(website, portnumber)
+	//var website string = "horizons.jpl.nasa.gov"
+	//var portnumber int = 6775
+	response, response_error = JPLProfile.communications.Telnet_connect()
 
 	if(response_error != nil) {
 	
--- go.mod
+++ go.mod
@@ -1,11 +1,12 @@
-module example.com/JPLTelnet
+module github.com/prestegard00000/JPLTelnet_go
 
 go 1.18
 
+require github.com/prestegard00000/telnet_interface_go v0.0.0 
 
-require example.com/telnet_interface.go v0.0.0-00010101000000-000000000000
+//require example.com/telnet_interface.go v0.0.0-00010101000000-000000000000
 
-replace example.com/telnet_interface.go => ../Telnet
+//replace example.com/telnet_interface.go => ../Telnet
 
 require (
 	github.com/reiver/go-oi v1.0.0 // indirect
